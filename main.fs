e9f7914009f4cce874ca19c6b28d32e88c5ad812
RadarBee <kevin.lanik@gmail.com>
2020-05-22 19:51:07 +0000

    Modified main to read from web and write to file

--- main.fs
+++ main.fs
@@ -1,6 +1,24 @@
 open System
+open System.IO
+open System.Net
+open System.Text
+
+let writeToFile filename buffer =
+  use fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write)
+  fs.Write(buffer, 0, buffer.Length)
+
+let readFromWeb url =
+  use c = new WebClient()
+  c.DownloadString(Uri(url))
+
+let GetBytes (s:string) = Encoding.ASCII.GetBytes(s)
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main args =
+  readFromWeb "http://www.google.com" |> GetBytes |> writeToFile "test.txt"
+  (*
+  let html = readFromWeb "http://www.google.com"
+  let buffer = GetBytes html
+  writeToFile "test.txt" buffer
+  *)
+  0
